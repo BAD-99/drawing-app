d16d49fe46836c2cd0097fc87d7ca84a88d67c50
BAD-99 <brandon.andrew.davis@gmail.com>
2024-08-21 03:26:25 -0600

    add click and touch handler to work on all devices

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("drawing-board");
+const toolbar = document.getElementById("toolbar");
+const ctx = canvas.getContext("2d");
 
 const canvasOffsetX = canvas.offsetLeft;
 const canvasOffsetY = canvas.offsetTop;
@@ -13,45 +13,85 @@ let lineWidth = 5;
 let startX;
 let startY;
 
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-});
-
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
-
-    if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
-    }
-    
-});
-
-const draw = (e) => {
-    if(!isPainting) {
-        return;
-    }
-
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = 'round';
-
-    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
-    ctx.stroke();
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-canvas.addEventListener('mousedown', (e) => {
-    isPainting = true;
-    startX = e.clientX;
-    startY = e.clientY;
+toolbar.addEventListener("click", (e) => {
+  if (e.target.id === "clear") {
+    clearCanvas();
+  }
 });
 
-canvas.addEventListener('mouseup', e => {
-    isPainting = false;
-    ctx.stroke();
-    ctx.beginPath();
+toolbar.addEventListener("change", (e) => {
+  if (e.target.id === "stroke") {
+    ctx.strokeStyle = e.target.value;
+  }
+
+  if (e.target.id === "lineWidth") {
+    lineWidth = e.target.value;
+  }
 });
 
-canvas.addEventListener('mousemove', draw);
+const draw = (x, y) => {
+  if (!isPainting) {
+    return;
+  }
+
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = "round";
+
+  ctx.lineTo(x - canvasOffsetX, y);
+  ctx.stroke();
+};
+
+const startPainting = (x, y) => {
+  isPainting = true;
+  startX = x;
+  startY = y;
+};
+
+const endPainting = () => {
+  isPainting = false;
+  ctx.stroke();
+  ctx.beginPath();
+};
+
+function handleClick(e, cb) {
+  cb(e.clientX, e.clientY);
+}
+
+function handleTouch(e, cb) {
+  //get average coords
+  console.log(typeof e.touches);
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < e.touches.length; i++) {
+    x += e.touches[i].clientX;
+    y += e.touches[i].clientY;
+  }
+  x = parseInt(x / e.touches.length);
+  y = parseInt(y / e.touches.length);
+  console.log([x, y]);
+  cb(x, y);
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  handleClick(e, startPainting);
+});
+
+canvas.addEventListener("mouseup", endPainting);
+
+canvas.addEventListener("mousemove", (e) => {
+  handleClick(e, draw);
+});
+
+canvas.addEventListener("touchstart", (e) => {
+  handleTouch(e, startPainting);
+});
+
+canvas.addEventListener("touchend", endPainting);
+
+canvas.addEventListener("touchmove", (e) => {
+  handleTouch(e, draw);
+});
